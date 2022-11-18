f47904f891125dca100afb66d86847be94be1434
Ade2002 <sikirusulaimon2002@gmail.com>
2022-11-18 23:32:47 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,10 @@
-import Head from 'next/head'
+import General from '../components/general/general'
 import Image from 'next/image'
 
 export default function Home () {
- 
   return (
-    <div className=''>
-   
-
-      <main className=''>
-       Ade is cool my boy dd as bggg kjhg
-      </main>
-
-      <footer className=''>
-        <a
-          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
-          target='_blank'
-          rel='noopener noreferrer'
-        >
-          Powered by{' '}
-          <span className=''>
-            <Image src='/vercel.svg' alt='Vercel Logo' width={72} height={16} />
-          </span>
-        </a>
-      </footer>
-    </div>
+    <General>
+      Ade is cool my boy dd as bggg kjhg
+    </General>
   )
 }
