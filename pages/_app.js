72ea2692e19111cd67d6fa7dbaae41ccbb74d606
Ade2002 <sikirusulaimon2002@gmail.com>
2022-11-19 10:24:14 +0100

    update

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 import '../styles/index.scss'
 function MyApp ({ Component, pageProps }) {
-=======
-import '../styles/globals.css'
-
-function MyApp({ Component, pageProps }) {
->>>>>>> cf28fccc378d4791d9acbd48496b47a618ed2c13
   return <Component {...pageProps} />
 }
 
