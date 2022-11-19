4b7621fd3637f2b0fb02e4e34d962683db886e29
Ade2002 <sikirusulaimon2002@gmail.com>
2022-11-19 10:11:41 +0100

    update

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home () {
   return (
     <General>
-      Ade is cool my boy dd as bggg kjhg bnn 
+      Ade is cool my boy dd as bggg kjhg bnn hd
     </General>
   )
 }
