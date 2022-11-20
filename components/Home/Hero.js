11e641209b32d94215fd82f5ae43d56e92f5a266
Ade2002 <sikirusulaimon2002@gmail.com>
2022-11-20 16:49:36 +0100

    update

--- components/Home/Hero.js
+++ components/Home/Hero.js
@@ -1,7 +1,7 @@
 /* eslint-disable @next/next/no-img-element */
-import LandingPageStyles from "../../styles/partial/home.module.scss";
-import LinkButtons from "../button/linkButton";
-import HeaderStyles from "../../styles/partial/header.module.scss";
+import LandingPageStyles from '../../styles/partial/home.module.scss'
+import LinkButtons from '../button/linkButton'
+import HeaderStyles from '../../styles/partial/header.module.scss'
 
 const Hero = () => {
   return (
@@ -10,13 +10,16 @@ const Hero = () => {
       id={`${LandingPageStyles.hero}`}
     >
       <div>
-        <span >Hello, <span className={`orange`}>I am</span></span>
-        <h1>
-          Sikiru Ademola
-        </h1>
+        <span>
+          Hello, <span className={`cOrange`}>I am</span>
+        </span>
+        <h1 className={`cOrange`}>Sikiru Ademola</h1>
+        <h3>Frontend Web Developer</h3>
         <p>
-          Acquire the 21st century skills so as to improve your relevance and
-          employability in this ever changing world.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Placerat nibh
+          nunc, id ac quisque. Vivamus vitae, id lacus et, enim mi amet. Leo
+          quis a ut est at sit ut aenean. Tincidunt nulla nisi, velit viverra
+          maecenas eu condimentum odio.
         </p>
         <LinkButtons
           classes={`${HeaderStyles.linkButton} ${HeaderStyles.bgBlack} wow animate__animated animate__fadeInDown`}
@@ -26,16 +29,16 @@ const Hero = () => {
       </div>
       <img
         className={`${LandingPageStyles.none_m}  wow animate__animated animate__fadeIn`}
-        src="/adeHero.png"
-        alt="adeHero"
+        src='/adeHero.png'
+        alt='adeHero'
       />
       <img
         className={`wow animate__animated animate__fadeIn`}
-        src="/adeHero.png"
-        alt="adeHero"
+        src='/adeHero.png'
+        alt='adeHero'
       />
     </section>
-  );
-};
+  )
+}
 
-export default Hero;
+export default Hero
