f3fcd85878dcbe8966d099222c60f7537a7c7890
Ade2002 <sikirusulaimon2002@gmail.com>
2022-11-21 09:07:19 +0100

    update

--- components/Home/Connect.js
+++ components/Home/Connect.js
@@ -1,7 +1,8 @@
 /* eslint-disable @next/next/no-img-element */
-import LandingPageStyles from "../../styles/partial/home.module.scss";
-import LinkButtons from "../button/linkButton";
-import HeaderStyles from "../../styles/partial/header.module.scss";
+import LandingPageStyles from '../../styles/partial/home.module.scss'
+import LinkButtons from '../button/linkButton'
+import HeaderStyles from '../../styles/partial/header.module.scss'
+import { motion } from 'framer-motion'
 
 const Connect = () => {
   return (
@@ -9,18 +10,49 @@ const Connect = () => {
       className={`${LandingPageStyles.flex_c} ${LandingPageStyles.section} `}
       id={`${LandingPageStyles.community}`}
     >
-      <h2>Our Community</h2>
-      <div className={`${LandingPageStyles.ademola}`}>
-        {/*   <img src='ademola.png' alt='ademola' /> */}
-        <i>We Nuture and build the future Tech Giants in Africa</i>
+      <div>
+        <motion.h2
+          initial={{ opacity: 0, y: 100 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 2 }}
+          className={`cOrange`}
+        >
+          Connect with me
+        </motion.h2>
+        <motion.p
+          initial={{ opacity: 0, y: 200 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 2 }}
+        >
+          The ants enjoyed the barbecue more than the family. Everyone was busy,
+          so I went to the movie alone.
+        </motion.p>
+        <div
+          className={`${LandingPageStyles.flex_cr} ${LandingPageStyles.buttons}`}
+        >
+          <LinkButtons
+            classes={`${HeaderStyles.linkButton} ${HeaderStyles.bgOrange} wow animate__animated animate__slideInUp`}
+            contents={`Hire me`}
+            to={`workstation`}
+          />
+          <LinkButtons
+            classes={`${HeaderStyles.linkButton} ${HeaderStyles.secondaryButton} wow animate__animated animate__slideInUp cOrange
+`}
+            contents={`Explore`}
+            to={`workstation`}
+          />
+        </div>
       </div>
-      <LinkButtons
-        classes={`${HeaderStyles.linkButton} myMargin wow animate__animated animate__fadeInDown`}
-        contents={`Join Community`}
-        to={`https://chat.whatsapp.com/LyFmrnDLM5u4FbQg366Ro9`}
+      <motion.img
+        initial={{ opacity: 0, y: '100px' }}
+        whileInView={{ opacity: 1, y: 0 }}
+        transition={{ duration: 3 }}
+        className={`wow animate__animated animate__slideInUp`}
+        src='myService.png'
+        alt='LaptopWomanFk'
       />
     </section>
-  );
-};
+  )
+}
 
-export default Connect;
+export default Connect
