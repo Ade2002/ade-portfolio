cb00764ebbea4e3fef5932f3e0a78982df3f744e
Ade2002 <sikirusulaimon2002@gmail.com>
2023-02-11 22:16:21 +0100

    update

--- pages/connect.js
+++ pages/connect.js
@@ -1,2 +1,20 @@
-import Connect from '../components/Home/Connect'
-rafce
+/* eslint-disable @next/next/no-img-element */
+import General from '../components/general/general'
+import ConnectWith from '../components/Home/Connect'
+
+export default function Connect () {
+    return (
+        <General>
+            <Connect /> 
+            {/* 
+        <Acad />
+        <StartLearn /> */}
+        {/* <AreYou />
+        */}
+        {/* <Courses /> */}
+        {/* <Testimonials /> */}
+        {/* <LearnWithUs /> */}
+        {/* <HowItWorks /> */}
+      </General>
+    )
+  }
